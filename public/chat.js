07aaa32c0d62e2858e335ce41a990562e29731eb
Eduardo Venegas <evenegas06@hotmail.com>
2023-04-02 15:01:15 -0600

    error on mounted method and add style file

--- public/chat.js
+++ public/chat.js
@@ -4,7 +4,38 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            nick: 'Eduardo'
-        }
+            step: 'nick',
+            nick: null,
+            message: null,
+            messages: [],
+        };
+    },
+    methods: {
+        send() {
+            socket.emit('chat-message', {
+                nick: this.nick,
+                message: this.message,
+                date: new Date().getTime()
+            });
+
+            this.message = null;
+        },
+        signIn() {
+            if (!this.nick) {
+                return;
+            }
+
+            this.step = 'chat';
+        },
+    },
+    mounted() {
+        socket.on('chat-message', (msg) => {
+            this.messages.push(msg);
+
+            setTimeout(() => {
+                const chatContainer = document.querySelector(".chat-container");
+                chatContainer.scrollTop = chatContainer.scrollHeight;
+            }, 10);
+        });
     }
 }).mount('#app');
